956e2234c4d799a0183423f9a9e2f498e257d27b
aqiyla <144479918+aqiyla@users.noreply.github.com>
2024-06-07 23:17:15 -0400

    Added GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,22 @@ export default function App() {
     <div className="App">
       <h1>Weather App</h1>
       <footer>
-        This project was coded by Aqiyla McLean and is open-sourced on GitHub.
+        This project was coded by{" "}
+        <a
+          href="https://github.com/aqiyla?tab=repositories"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Aqiyla McLean
+        </a>
+        {""} and is{" "}
+        <a
+          href="https://github.com/aqiyla/react-weather-app-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub.
+        </a>{" "}
       </footer>
     </div>
   );
