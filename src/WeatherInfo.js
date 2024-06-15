9e83e5b81a8b141e10c1d863f0d8f3d10f8151c3
aqiyla <144479918+aqiyla@users.noreply.github.com>
2024-06-15 01:24:24 -0400

    Created new CSS component

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -2,6 +2,7 @@ import React from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherIcon from "./WeatherIcon";
 import WeatherTemperature from "./WeatherTemperature";
+import "./WeatherInfo.css";
 
 export default function WeatherInfo(props) {
   return (
@@ -16,16 +17,20 @@ export default function WeatherInfo(props) {
             <li className="text-capitalize">{props.data.description}</li>
           </ul>
         </div>
-        <div className="col-3">
+        <div className="col-3 weather-details">
           <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {Math.round(props.data.wind)} mph</li>
+            <li>
+              Humidity: <strong>{props.data.humidity}%</strong>
+            </li>
+            <li>
+              Wind: <strong>{Math.round(props.data.wind)} mph</strong>
+            </li>
           </ul>
         </div>
       </div>
       <h2>
         <div className="weather-info d-flex justify-content-center">
-          <span>
+          <span className="weather-forecast-icon">
             {" "}
             <WeatherIcon code={props.data.icon} size={52} />
           </span>
